9e3a37f8a51060373b08e5d591aeb532cca4d0d9
Ishan <ishanwijes@gmail.com>
2022-12-07 02:57:03 +1100

    added changes to deploy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://stormy-forest-83840.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
